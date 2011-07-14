e354f04fa5ea191f3cb17f06d59cc626788a80d9
JIucToyxuu <antonduzhnov.90@gmail.com>
2011-07-14 10:41:38 +0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'ffaker', '1.8.0'
+
+gem 'sqlite3', '1.3.3'
+gem 'simple-rss' , '1.2.3'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+
+
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rss::Application.config.secret_token = '721602e59506ac17a78104b42b9b1f992acddcb766fd02cf11e8a52ad1bbf6f01f94c2c3af23e3919e980246a31fef51a30071517019b8b0d5636c48bb722687'
